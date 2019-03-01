2f88ca3b4009a0c6b115ea452a7b5944666db6b6
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-03-01 13:55:48 +1100

    method parm extractor

--- grapheign-core/src/main/java/com/grapheign/core/DataFetchingEnvMethodArgsExtractorImpl.java
+++ grapheign-core/src/main/java/com/grapheign/core/DataFetchingEnvMethodArgsExtractorImpl.java
@@ -1,11 +1,20 @@
 package com.grapheign.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.schema.DataFetchingEnvironment;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class DataFetchingEnvMethodArgsExtractorImpl implements DataFetchingEnvMethodArgsExtractor {
     private static final Object[] EMPTY_ARGS = new Object[0];
+    private final ObjectMapper mapper;
+
+    public DataFetchingEnvMethodArgsExtractorImpl(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
 
     public Object[] extractArguments(Method method, DataFetchingEnvironment environment) {
         Class<?>[] parameterTypes = method.getParameterTypes();
@@ -13,7 +22,30 @@ public class DataFetchingEnvMethodArgsExtractorImpl implements DataFetchingEnvMe
             return EMPTY_ARGS;
         }
 
-        // FIXME
-        return EMPTY_ARGS;
+        //so far the simplest case only: parameters in graphql should match method parameter names, order and types.
+        //TODO add conversion/default values, graphql-java agrs mapping support
+        ArrayList<Object> args = new ArrayList<>();
+        Map<String, Object> arguments = environment.getArguments();
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            Class parmType = parameterTypes[i];
+            String parmName = getParmName(parameters[i]);
+            Object converted = mapper.convertValue(arguments.get(parmName), parmType);
+            args.add(i, converted);
+        }
+
+        return args.toArray();
     }
+
+    //to get parameter.getName() the javac option '-parameters' or '-g' is required. we can't force users to
+    // use these options. So, let's do our best to find parm names:
+    // - get parm name from  'GrapheignType' annotation on parm
+    // - if debug info is present and there is no GrapheignType annotation on parm need to use parm name from debug info
+    // - if none of them present let's rely just on order of parms.
+    //TODO implement all possible ways to get parm name. for now let's rely on debug info only.
+    private String getParmName(Parameter parameter) {
+        return parameter.getName();
+    }
+
+
 }
--- grapheign-core/src/test/java/com/grapheign/core/DataFetchingEnvMethodArgsExtractorImplTest.java
+++ grapheign-core/src/test/java/com/grapheign/core/DataFetchingEnvMethodArgsExtractorImplTest.java
@@ -1,15 +1,20 @@
 package com.grapheign.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.schema.DataFetchingEnvironment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DataFetchingEnvMethodArgsExtractorImplTest {
@@ -18,7 +23,8 @@ class DataFetchingEnvMethodArgsExtractorImplTest {
 
     @BeforeEach
     void setUp() {
-        extractor = new DataFetchingEnvMethodArgsExtractorImpl();
+        ObjectMapper mapper = new ObjectMapper();
+        extractor = new DataFetchingEnvMethodArgsExtractorImpl(mapper);
     }
 
     @Test
@@ -26,10 +32,48 @@ class DataFetchingEnvMethodArgsExtractorImplTest {
         Method method = TestClass.class.getMethod("noArgsMethod");
         assertEquals(0, extractor.extractArguments(method, environment).length);
     }
+
+    @Test
+    public void shouldConvertGraphqlParmsToSingleMethodParms(@Mock DataFetchingEnvironment environment) throws NoSuchMethodException {
+        Map<String, Object> graphqlParms = new HashMap<String, Object>() {{
+            put("reallySimpleParm", "expectedValue");
+        }};
+
+        when(environment.getArguments()).thenReturn(graphqlParms);
+
+        Method method = TestClass.class.getMethod("singlePrimitiveParm", String.class);
+        Object[] actual = extractor.extractArguments(method, environment);
+        Object[] expected = {"expectedValue"};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldConvertGraphqlParmsToMultipleMethodParms(@Mock DataFetchingEnvironment environment) throws NoSuchMethodException {
+        Map<String, Object> graphqlParms = new HashMap<String, Object>() {{
+            put("stringParm1", "value1");
+            put("stringParm2", "value2");
+        }};
+
+        when(environment.getArguments()).thenReturn(graphqlParms);
+
+        Method method = TestClass.class.getMethod("fewSimplePArmsOfTheSameType", String.class, String.class);
+        Object[] actual = extractor.extractArguments(method, environment);
+        Object[] expected = {"value1", "value2"};
+        assertArrayEquals(expected, actual);
+    }
 }
 
 class TestClass {
     public Object noArgsMethod() {
         return new Object();
     }
+
+    public Object singlePrimitiveParm(String reallySimpleParm) {
+        return new Object();
+    }
+
+    public Object fewSimplePArmsOfTheSameType(String stringParm1, String stringParm2 ) {
+        return new Object();
+    }
+
 }
