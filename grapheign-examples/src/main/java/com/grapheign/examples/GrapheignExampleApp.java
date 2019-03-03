814800f76f3bb9f71ecf1ea94ecba09625283bc1
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-03-03 22:13:14 +1100

    clean up

--- grapheign-examples/src/main/java/com/grapheign/examples/GrapheignExampleApp.java
+++ grapheign-examples/src/main/java/com/grapheign/examples/GrapheignExampleApp.java
@@ -1,10 +1,8 @@
 package com.grapheign.examples;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,11 +10,4 @@ public class GrapheignExampleApp {
     public static void main(String[] args) {
         SpringApplication.run(GrapheignExampleApp.class, args);
     }
-
-/*    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enableDefaultTyping();
-        return mapper;
-    }*/
 }
