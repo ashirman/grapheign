c592cbeb46c4da654a60b44b667aeea034a54d56
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-03-03 22:15:03 +1100

    implement GraphQLProvider using wiring given by grapheign

--- grapheign-examples/src/main/java/com/grapheign/examples/graphql/GraphQLProvider.java
+++ grapheign-examples/src/main/java/com/grapheign/examples/graphql/GraphQLProvider.java
@@ -2,23 +2,40 @@ package com.grapheign.examples.graphql;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import com.grapheign.core.GraphqlTypeFetcherSupplier;
+import com.grapheign.examples.FeignGraphqlTypeFetcherSupplier;
 import graphql.GraphQL;
+import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.RuntimeWiring.Builder;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.schema.idl.TypeRuntimeWiring;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import static com.google.common.io.Resources.getResource;
 import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+import static java.util.stream.Collectors.toMap;
 
-//@Component
+@Component
 public class GraphQLProvider {
+    private final GraphqlTypeFetcherSupplier graphqlTypeFetcherSupplier;
+
+    @Autowired
+    public GraphQLProvider(GraphqlTypeFetcherSupplier graphqlTypeFetcherSupplier) {
+        this.graphqlTypeFetcherSupplier = graphqlTypeFetcherSupplier;
+    }
 
     private GraphQL graphQL;
 
@@ -34,10 +51,18 @@ public class GraphQLProvider {
     }
 
     private RuntimeWiring buildWiring() {
-        return RuntimeWiring.newRuntimeWiring()
-                .type(newTypeWiring("Query")
-                        .build())
-                .build();
+        Builder wiring = RuntimeWiring.newRuntimeWiring();
+
+        graphqlTypeFetcherSupplier.get().forEach((type, fieldDataFetchers) -> wiring.type(typeRuntimeWiring(type, fieldDataFetchers)));
+
+        //wiring.type(newTypeWiring("Character").typeResolver((env) -> env.))
+
+        return wiring.build();
+    }
+
+    private TypeRuntimeWiring typeRuntimeWiring(String typename, Collection<Entry<String, DataFetcher>> fieldsDataFetchers) {
+        Map<String, DataFetcher> fetchersMap = fieldsDataFetchers.stream().collect(toMap(Entry::getKey, Entry::getValue));
+        return newTypeWiring(typename).dataFetchers(fetchersMap).build();
     }
 
     @Bean
