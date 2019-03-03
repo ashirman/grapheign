76782087d1853ded3765341374106d35673f7290
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-03-03 22:16:28 +1100

    add grapheign annotation to feign client

--- grapheign-examples/src/main/java/com/grapheign/examples/feign/StarWarFeignClient.java
+++ grapheign-examples/src/main/java/com/grapheign/examples/feign/StarWarFeignClient.java
@@ -1,5 +1,7 @@
 package com.grapheign.examples.feign;
 
+import com.grapheign.core.annotations.GrapheignField;
+import com.grapheign.core.annotations.GrapheignQuery;
 import com.grapheign.examples.types.Character;
 import com.grapheign.examples.types.Droid;
 import com.grapheign.examples.types.Episode;
@@ -12,16 +14,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @FeignClient(name = "startwarclient", url = "${starwar.url}")
+@GrapheignQuery
 public interface StarWarFeignClient {
+    @GrapheignField(name = "hero")
     @RequestMapping(value = "/hero", method = GET)
     Character hero(@RequestParam("episode") Episode episode);
 
+    @GrapheignField(name = "human")
     @RequestMapping(value = "/human/{id}", method = GET)
     Human human(@PathVariable("id") String id);
 
+    @GrapheignField(name = "droid")
     @RequestMapping(value = "/droid/{id}", method = GET)
     Droid droid(@PathVariable("id") String id);
 
+    @GrapheignField(name = "character")
     @RequestMapping(value = "/character/{id}", method = GET)
     Character character(@PathVariable("id") String id);
 }
